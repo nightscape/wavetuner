dd6281edeeb25b17a12afda053a6aeb4fb725479
Martin Mauch <mauch@crealytics.de>
2012-12-03 22:56:05 +0100

    Calabash runs. Test successfully fails ;)

---
+++ features/support/app_life_cycle_hooks.rb
@@ -0,0 +1,17 @@
+require 'calabash-android/management/adb'
+require 'calabash-android/operations'
+include Calabash::Android::Operations
+
+Before do |scenario|
+  start_test_server_in_background
+end
+
+After do |scenario|
+  if scenario.failed?
+    screenshot_embed
+  end
+end
+
+After do
+    shutdown_test_server
+end
